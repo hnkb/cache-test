d27a46c7941cc2ab9264a4837155561edee6719b
Hani Kaabi <hani@hnkb.de>
2023-10-05 06:44:43 +0200

    Send images with cache headers and from different URLs

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 
-const count = 300;
+const count = (new URL(location.href).searchParams.get('count')) || 300;
 
+const start = performance.now();
 performance.setResourceTimingBufferSize(count + 100);
 
 
@@ -9,7 +10,7 @@ const promises = [];
 
 for (var i = 0; i < count; i++) {
 	const img = document.createElement('img');
-	img.src = 'img.jpg';
+	img.src = `/img/${i}`;
 	container.appendChild(img);
 
 	promises.push(new Promise(resolve => img.onload = () => resolve()));
@@ -25,9 +26,9 @@ const totalDuration = images.reduce((sum, r) => sum += r.duration, 0);
 const toMegabytes = size => Math.round(10 * size / 1024 / 1024) / 10;
 
 document.querySelector('.img-count').innerHTML = images.length;
-document.querySelector('.img-duration').innerHTML = Math.round(10 * totalDuration) / 10 + ' ms';
+document.querySelector('.img-duration').innerHTML = Math.round(performance.now() - start) + ' ms';
 document.querySelector('.img-size').innerHTML =
-	`<strong>${toMegabytes(totalSize)} MB</strong>
-      (from a total of <strong>${toMegabytes(1171927 * images.length)} MB</strong>)`;
+	`transferred <strong>${toMegabytes(totalSize)} MB</strong>
+      from a total of <strong>${toMegabytes(1171927 * count)} MB</strong>`;
 
 console.log(images);
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 
 import Express from 'express';
+import { fileURLToPath } from 'url';
 
 
 const app = Express();
 
 app.disable('x-powered-by')
 app.use(Express.static('client'));
+
+app.get('/img/:id', (req, res) => {
+	res.header('Cache-Control', 'public, max-age=31560000, immutable');
+	res.sendFile(fileURLToPath(new URL('./client/img.jpg', import.meta.url)));
+});
 
 app.get('*', (req, res) => { res.status(404); });
 
